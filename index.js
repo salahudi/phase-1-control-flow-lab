94ca0ac3095c06a152b6fefe9255514f7d38f84d
salahudin <salahudin.salat@student.moringaschool.com>
2022-08-16 21:04:40 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  let result
+  if (ride <= 400){
+    result = 'This one is on me!'
+  }  else if (ride > 2500){
+    result = 'No can do.'
+  } else if (ride > 2000){
+    result = 'I will gladly take your thirty bucks.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city = "NYC") {
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  // Write your code here
+  switch (tip) {
+    case "generous":
+      return 'Thank you so much.'
+    case "not as generous":
+      return "Thank you." 
+    default:
+      return "Bye." 
+ }
 }
